a8c9bc8cd9605afc0e09418618568777f845762c
Mateo <malvarez@devsys.com.uy>
2022-06-14 13:56:06 -0300

    test

--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -2,18 +2,30 @@ import classNames from 'classnames'
 import { ButtonProps } from './Button.types'
 
 import styles from './Button.module.css'
+import { useState } from 'react'
 
-const handleClick = ({ onClick }: { onClick: ButtonProps['onClick']}) => (e: any) => {
+const handleClick = ({ onClick }: { onClick: ButtonProps['onClick'] }) => (e: any) => {
   onClick!(e)
 }
 
 const Button = ({ children, onClick, variant }: ButtonProps) => {
-    return <button 
-      onClick={onClick && handleClick({ onClick })}
-      className={classNames(styles.button, {
-        [styles[`variant-${variant}`]]: variant
-      })}
-    >{children}</button>
+  const [state, setState] = useState(false)
+
+  return (
+    <>
+      <button
+        onMouseEnter={() => setState(true)}
+        onMouseLeave={() => setState(false)}
+        onClick={onClick && handleClick({ onClick })}
+        className={classNames(styles.button, {
+          [styles[`variant-${variant}`]]: variant
+        })}
+      >click lala: {children}</button>
+      {
+        state && <div>lalalala</div>
+      }
+    </>
+  )
 }
 
 export default Button
